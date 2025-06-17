7fad7a7c1afb973548aaf6fd26a0a66a9d0af06c
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-06-17 00:47:52 -0300

    :sparkles: feat: GenericDAO

--- src/main/java/edu/br/ifpr/dao/GenericDAO.java
+++ src/main/java/edu/br/ifpr/dao/GenericDAO.java
@@ -4,10 +4,58 @@
  */
 package edu.br.ifpr.dao;
 
+import edu.br.ifpr.util.PersistenceUtil;
+import jakarta.persistence.EntityManager;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+
 /**
  *
  * @author rafael
  */
-public class GenericDAO {
-    
+public class GenericDAO<PK, T> {
+
+    private EntityManager em;
+
+    public GenericDAO() {
+        em = PersistenceUtil.getEntityManager();
+    }
+
+    public void create(T entity) {
+        em.getTransaction().begin();
+        em.persist(entity);
+        em.getTransaction().commit();
+    }
+
+    public T retrieve(PK pk) {
+        T t = (T) em.find(getTypeClass(), pk);
+        return t;
+    }
+
+    public Class<?> getTypeClass() {
+        Class<?> clazz = (Class< ?>) ((ParameterizedType) this.getClass()
+                .getGenericSuperclass())
+                .getActualTypeArguments()[1];
+        return clazz;
+    }
+
+    public void update(T entity) {
+        em.getTransaction().begin();
+        em.merge(entity);
+        em.getTransaction().commit();
+    }
+
+    public void delete(T entity) {
+        em.getTransaction().begin();
+        em.remove(em);
+        em.getTransaction().commit();
+    }
+
+    public List<T> findAll() {
+        List<T> entities = em.createQuery("FROM " + getTypeClass()
+                .getName())
+                .getResultList();
+        return entities;
+    }
+
 }
